9240a59de2f91c64dae83c3917729d94ecfcaca5
Thomas Beckford <tebeckford@gmail.com>
2023-08-15 23:43:23 -0300

    feat: simple landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,18 @@ import Contact from "../components/Templates/Contact";
 import Footer from "../components/Templates/Footer";
 import Partners from "../components/Templates/Partners";
 import Solutions from "../components/Templates/Solutions";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Simple landing</title>
+        <meta property="og:title" content="Titulo social de la landing" />
+        <meta property="og:description" content="Descripcion social de la landing" />
+        <meta name="description" content="Descripcion de landing" key="desc" />
+        <meta property="og:image" content="https://example.com/images/cool-page.jpg" />
+      </Head>
       <Navbar />
       <Main />
       <Solutions />
